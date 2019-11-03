24ae7ac811fe8c8d7844338b5c057447c7344d5b
unknown <bgg269@myy.haaga-helia.fi>
2019-11-03 08:32:53 +0200

    junit5, tests dont work

--- WineListProject/src/test/java/com/example/WineListProject/CategoryRepositoryTest.java
+++ WineListProject/src/test/java/com/example/WineListProject/CategoryRepositoryTest.java
@@ -5,16 +5,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.WineListProject.domain.Category;
 import com.example.WineListProject.domain.CategoryRepository;
 
 
-@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class CategoryRepositoryTest {
 
 	@Autowired
--- WineListProject/src/test/java/com/example/WineListProject/WineRepositoryTest.java
+++ WineListProject/src/test/java/com/example/WineListProject/WineRepositoryTest.java
@@ -6,16 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.WineListProject.domain.Category;
 import com.example.WineListProject.domain.Wine;
 import com.example.WineListProject.domain.WineRepository;
 
-@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class WineRepositoryTest {
 
 	@Autowired
